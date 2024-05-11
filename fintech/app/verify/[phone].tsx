ec56cdbc514be3342eebd15cb20d3964ce207892
Mindless-Magnet <muhammedshajahan45@gmail.com>
2024-05-11 10:06:58 +0530

    rebuilt app

---
+++ fintech/app/verify/[phone].tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { useLocalSearchParams } from 'expo-router';
+
+const Page = () => {
+  const { phone, signin } = useLocalSearchParams<{ phone: string; signin: string }>();
+
+
+  return (
+    <View>
+      <Text>Page</Text>
+    </View>
+  )
+}
+
+export default Page
